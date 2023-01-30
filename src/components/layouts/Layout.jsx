ad395fdd5760c77e76c5100dbccc14cb849a8e3d
jeffigy <evangelistajeffy25@gmail.com>
2023-01-30 10:03:10 +0800

    first commit

---
+++ src/components/layouts/Layout.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Body from './Body'
+import { Greet } from 'components/learning/Greet'
+function layout() {
+    return (
+        <>
+            <Body>
+                <Greet />
+            </Body>
+        </>
+    )
+}
+
+export default layout
---
+++ src/components/learning/Greet.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Greet = (props) => {
+    const name = props.name;
+    return (
+        <div>Hello {name}</div>
+    )
+}
+
+// export default Greet
