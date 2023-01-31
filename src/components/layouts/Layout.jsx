5998804499fb3541059b887d555b28612ba19835
jeffigy <evangelistajeffy25@gmail.com>
2023-01-31 09:44:41 +0800

    5th commit

--- src/components/layouts/Layout.jsx
+++ src/components/layouts/Layout.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useTransition } from 'react'
 import Body from './Body'
 import { Greet } from 'components/learning/Greet'
 import { Message } from 'components/learning/Message'
@@ -7,19 +7,37 @@ import { GreetParentComponent } from 'components/learning/GreetParentComponent'
 import List from 'components/learning/List'
 import { Form } from 'components/learning/Form'
 import { PostList } from 'components/learning/PostList'
+import NAMES from 'components/learning/data.json'
+
+
 function Layout() {
-    const name = 'Doe'
+    const [isPending, startTransition] = useTransition()
+    const [inputValue, setInputValue] = useState('')
+    const [query, setQuery] = useState('')
+    const changeHandler = (e) => {
+        setInputValue(e.target.value);
+        startTransition(() => setQuery(e.target.value))
+    }
+    const filterNames = NAMES.filter(item => {
+        return item.first_name.includes(query) || item.last_name.includes(query)
+    })
     return (
         <>
             <Body>
                 {/* <Greet /> */}
-                {/* <Message name={name} />
-                 */}
-                 {/* <ClickHandler/> */}
-                 {/* <GreetParentComponent/> */}
-                 {/* <List/> */}
-                 {/* <Form/> */}
-                 <PostList/>
+                {/* <Message name={name} />*/}
+                {/* <ClickHandler/> */}
+                {/* <GreetParentComponent/> */}
+                {/* <List/> */}
+                {/* <Form/> */}
+                {/* <PostList/> */}
+                <input type="text" value={inputValue} onChange={changeHandler} />
+                {isPending && <p>Updating List...</p>}
+                {filterNames.map((item) => {
+                    return (
+                        <p key={item.id}>{item.first_name} {item.last_name}</p>
+                    )
+                })}
             </Body>
         </>
     )
